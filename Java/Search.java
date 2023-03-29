e167da922ad69140fb61d8a2e4e3535306673678
Kell Grady <kellgrady@gmail.com>
2023-03-29 16:14:54 -0400

    Starting Search

---
+++ Java/Search.java
@@ -0,0 +1,12 @@
+package Java;
+import java.sql.*;
+import java.util.*;
+
+public class Search {
+    private String searchInput = null;
+
+    Search(String in){
+        this.searchInput = in;
+
+    }
+}

fadb59be1f550e3f88a84fd3c7c4c742790e8bad
emmas04 <emmastrang6@gmail.com>
2023-04-10 16:04:37 -0400

    update user.java

--- Java/User.java
+++ Java/User.java
@@ -1,9 +1,29 @@
 package Java;
+
 import java.sql.*;
 import java.util.*;
 
 public class User {
+    Connection connect = null;
     private boolean favorite;
     private String user_name;
     private String user_bio;
+
+    public User(boolean favorite, String user_name, String user_bio) {
+        this.favorite = favorite;
+        this.user_name = user_name;
+        this.user_bio = user_bio;
+    }
+
+    public Boolean setFavorite(Boolean favorite) {
+        return this.favorite = favorite;
+    }
+
+    public String setUserName(String user_name) {
+        return this.user_name = user_name;
+    }
+
+    public String setUserBio(String user_bio) {
+        return this.user_bio = user_bio;
+    }
 }

7d18726df603c3bd7dc46d33f9ade7be1c8e3929
LonerMon <jomm@clarkson.edu>
2023-04-12 16:10:48 -0400

    Update Edit.java

---
+++ Java/Edit.java
@@ -0,0 +1,54 @@
+package Java;
+
+import java.sql.*;
+import java.util.*;
+
+public class Edit {
+    private Connection connect = null;
+    private SQLConnect database;
+    private int recipeId;
+    private String recipeName;
+    private List<String> ingredients;
+    private String instructions;
+    private List<String> utensils;
+    private int chefRate;
+    private int prepTime;
+    private int serveSize;
+    private int oTemp;
+    private String pic;
+
+    public Edit(SQLConnect Data, int rId, String rN, List<String> iG, String iN, List<String> uT, int cR, int pT, int sS, int oT, String pC) {
+        this.database = Data;
+        this.connect = database.getConn();
+        this.recipeId = rId;
+        this.recipeName = rN;
+        this.ingredients = iG;
+        this.instructions = iN;
+        this.utensils = uT;
+        this.chefRate = cR;
+        this.prepTime = pT;
+        this.serveSize = sS;
+        this.oTemp = oT;
+        this.pic = pC;
+    }
+
+    public void Update() throws SQLException {
+        try {
+            PreparedStatement ps = connect.prepareStatement("Edit recipebuddy.recipes (recipeName, ingredients, instructions, utensils, chefRate, prepTime, serveSize, oTemp, pic) values (?, ?, ?, ?, ?, ?, ?, ?, ?)");
+            ps.setString(1, recipeName);
+            ps.setString(2, String.join(" ", ingredients));
+            ps.setString(3, instructions);
+            ps.setString(4, String.join(" ", utensils));
+            ps.setInt(5, chefRate);
+            ps.setInt(6, prepTime);
+            ps.setInt(7, serveSize);
+            ps.setInt(8, oTemp);
+            ps.setString(9, pic);
+            ps.setInt(10, recipeId);
+            ps.executeUpdate();
+            ps.close();
+        } catch (SQLException e) {
+            System.out.println(e.toString());
+        }
+    }
+}

f4dbdef4f490743587dfaccea24508a7092ec7c4
LonerMon <jomm@clarkson.edu>
2023-04-14 12:35:20 -0400

    Update main.java

--- Java/main.java
+++ Java/main.java
@@ -25,5 +25,22 @@ public static void main(String[] args) throws Exception {
         Test.search("Name", true, false);
         Test.search("Name", false, true);
         Test.resetDB();
+
+        //Test for endTime
+
+        // Define the updated recipe information
+        String updatedRecipeName = "Spaghet";
+        List<String> updatedIngredients = Arrays.asList("spaghet", "meatbol", "sauce", "onion", "garlic");
+        String updatedInstructions = "1. Cook spaghet\n2. In a large pan, add onion and garlic. \n3. Add meatbol. \n4. Add sauce. \n5. Serve spaghet.";
+        List<String> updatedUtensils = Arrays.asList("large pan", "spatula", "pot");
+        int updatedChefRate = 4;
+        int updatedPrepTime = 20;
+        int updatedServeSize = 4;
+        int updatedOTemp = 0;
+        //String updatedPic = "spaghet.jpg";
+    
+        // Create an instance of the Edit class and update the recipe in the database
+        Edit edit = new Edit(Database, updatedRecipeName, updatedIngredients, updatedInstructions, updatedUtensils, updatedChefRate, updatedPrepTime, updatedServeSize, updatedOTemp);
+        edit.Update();
     }
 }

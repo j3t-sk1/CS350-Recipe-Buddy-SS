ba4a58370864fa51cd6449f363d41331e90adf1f
emmas04 <emmastrang6@gmail.com>
2023-04-15 16:40:58 -0400

    updating user.java

--- Java/User.java
+++ Java/User.java
@@ -8,22 +8,32 @@ public class User {
     private boolean favorite;
     private String user_name;
     private String user_bio;
+    private String user_picture;
+    private List<String> user_recipes;
+    private List<String> pantry;
 
-    public User(boolean favorite, String user_name, String user_bio) {
-        this.favorite = favorite;
-        this.user_name = user_name;
-        this.user_bio = user_bio;
+    public User(boolean fav, String userName, String userBio, String userPic, List<String> userRecipes, List<String> p) {
+        this.favorite = fav;
+        this.user_name = userName;
+        this.user_bio = userBio;
+        this.user_picture = userPic;
+        this.user_recipes = userRecipes;
+        this.pantry = p;
     }
 
-    public Boolean setFavorite(Boolean favorite) {
-        return this.favorite = favorite;
+    public void Create(int size) throws SQLException {
+        try {
+            PreparedStatement ps = connect.prepareStatement
+            ("insert into recipebuddy.users (favorite, user_name, user_bio, user_picture, user_recipes, pantry) values (?, ?, ?, ?, ?, ?)");
+            ps.setBoolean(1, favorite);
+            ps.setString(2, user_name);
+            ps.setString(3, user_bio);
+            ps.setString(4, user_picture);
+            ps.setString(5, String.join(" ", user_recipes));
+            ps.setString(6, String.join(" ", pantry));
+        } catch (Exception e){
+            System.out.println(e.toString());
+        }
     }
 
-    public String setUserName(String user_name) {
-        return this.user_name = user_name;
-    }
-
-    public String setUserBio(String user_bio) {
-        return this.user_bio = user_bio;
-    }
 }

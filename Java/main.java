f47e8bea95bf937b97eb520d2bed953d0b6f0b0d
cpsoucy <96217410+cpsoucy@users.noreply.github.com>
2023-04-15 16:51:05 -0400

    removing id from recipes sql call

--- Java/main.java
+++ Java/main.java
@@ -24,7 +24,7 @@ public static void main(String[] args) throws Exception {
         Test.printDB();
         
 
-        Test.resetDB();
+        // Test.resetDB();
     }
 }
 //String updatedRecipeName = "Spaghet";

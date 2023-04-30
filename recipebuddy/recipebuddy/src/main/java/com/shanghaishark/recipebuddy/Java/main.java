a335bf15356a50dae681f543d5a685c4a6de6eab
Kell Grady <kellgrady@gmail.com>
2023-04-30 15:53:12 -0400

    Testing For ServeScale

--- recipebuddy/recipebuddy/src/main/java/com/shanghaishark/recipebuddy/Java/main.java
+++ recipebuddy/recipebuddy/src/main/java/com/shanghaishark/recipebuddy/Java/main.java
@@ -14,27 +14,29 @@ public static void main(String[] args) throws Exception {
         //Add Test
         //For add String
         //(Name (Ingredient Ingredient) (In stru ctions) (Utensil Utensil) rating preptime servesize oTemp)
-        String testaddS = "Pizza (Cheese Tomato) (cook lol) (rolling_pin oven) 5 40 2 400";
-        Test.testadd();
-        // Test.testadd(testaddS);
-        //Test.printDB();
+      String testaddS = "Pizza (Cheese Tomato) (Add 2 cups Cheese and then add 4 scoops Tomato) (rolling_pin oven) 5 40 2 400";
+    //    Test.testadd();
+     Test.testadd(testaddS);
+    //     //Test.printDB();
 
-        Test.search("Name", false, true);
-        Test.search("Name", true, false);
-        //Edit Test
-        // Test.testedit();
-        // Test.printDB();
+    //     Test.search("Name", false, true);
+    //     Test.search("Name", true, false);
+    //     //Edit Test
+    //     // Test.testedit();
+    //     // Test.printDB();
         
-        //User Test
-        //String testUserString = "true username exampleBio picture (pizza pasta) (utensils allergens)";
-        //Test.testUser();
+    //     //User Test
+    //     //String testUserString = "true username exampleBio picture (pizza pasta) (utensils allergens)";
+    //     //Test.testUser();
         
-        //Test.printDBU();
+    //     //Test.printDBU();
         
 
-       ImageHandler test = new ImageHandler();
-       System.out.println(test.addUrl("chicken", "rice"));
-
+    //    ImageHandler test = new ImageHandler();
+    //    System.out.println(test.addUrl("chicken", "rice"));
+        ServeScaler scale = new ServeScaler(Database);
+        //You will have to manually put in indx, first argument
+        scale.Try2(0, 4);
        Test.resetDB();
     }
 }
